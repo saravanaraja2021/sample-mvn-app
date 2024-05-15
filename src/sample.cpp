483f15957a26abd4f7119984fd4a43aadd0b5e4c
saravanaraja2021 <76823910+saravanaraja2021@users.noreply.github.com>
2024-05-15 18:17:31 +0530

    Update sample.cpp

--- src/sample.cpp
+++ src/sample.cpp
@@ -1,14 +1,19 @@
-// C++ program to display "Hello World"
-
-// Header file for input output functions
 #include <iostream>
-using namespace std;
+#include <memory>
 
-// Main() function: where the execution of
-// program begins
-int main()
-{
-  // Prints hello world
-    cout << "Hello World";
-    return 0;
+class Test {
+  typedef std::string String;
+  String name;
+
+  public:
+    Test(String iname) : name(iname){}
+    String getName(){
+      return name;
+    }
+};
+
+int main(){
+  auto t = std::make_unique<Test>("Terminal Root");
+  std::cout << t->getName() << '\n';
+  return 0;
 }
